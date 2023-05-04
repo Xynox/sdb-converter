a9c574fe733811c3d2f93ff0b12193ea52373892
Xynox <joel.wittke06@gmail.com>
2023-05-04 16:13:06 +0200

    Add install methods
    This commit adds install methods inside install.py and imports them into
    main.py. The install checks for the directory and skips the creation, if
    it already exists. Both input and output folders named "import_dateien"
    and "export_dateien" need to be there for the upcoming development.

--- install.py
+++ install.py
@@ -1,2 +1,29 @@
 # todo: this will be a script creating a folder for input/output
 # it could also be used as a way to give the user feedback on what files are missing etc.
+import os
+
+
+def createImportFolder():
+    parentDir = './'
+    dirName = "import_dateien"
+    fullPath = os.path.join(parentDir, dirName)
+
+    if os.path.exists(fullPath):
+        print("Import-Verzeichnis existiert bereits, überspringe Erstellung")
+
+    else:
+        os.mkdir(fullPath)
+        print("Kein Import-Verzeichnis erkannt, wird neu erstellt")
+
+
+def createExportFolder():
+    parentDir = './'
+    dirName = "export_dateien"
+    fullPath = os.path.join(parentDir, dirName)
+
+    if os.path.exists(fullPath):
+        print("Export-Verzeichnis existiert bereits, überspringe Erstellung")
+
+    else:
+        os.mkdir(fullPath)
+        print("Kein Export-Verzeichnis erkannt, wird neu erstellt")
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 # todo: here will be the main function of the converter
+
+from install import *
